7fab601ede6d901d4547688d1b51305f1d2e2e63
Andrikin <contasecretaalternativa@gmail.com>
2026-06-12 18:47:50 -0300

    wip: testar instalador gvim!!

--- gvim-installer.bat
+++ gvim-installer.bat
@@ -1,41 +1,41 @@
 :: DEPENDENCIA: git e curl
 @echo off
 
-setlocal
+@setlocal
+
+set GITVERSION=2.54.0
+set GVIMVERSION=9.2.0612
 
 where git > NUL 2> NUL
 
-if %ERRORLEVEL% EQU 0 (
+if "%ERRORLEVEL%" EQU 0 (
     echo "Encontrado executável git"
 ) else (
     REM install git
     set GITDIR=%USERPROFILE%\Documents\git\
-    set GITZIP=MinGit-2.54.0-64-bit.zip
-    set GITLINK=https://github.com/git-for-windows/git/releases/download/v2.54.0.windows.1/MinGit-2.54.0-64-bit.zip
-    if not exist %GITDIR% (
-        mkdir %GITDIR%
-    )
+    set GITLINK=https://github.com/git-for-windows/git/releases/download/v%GITVERSION%.windows.1/MinGit-%GITVERSION%-64-bit.zip
+    for /F "delims=" %%i in ("%GITLINK%") do set GITZIP=%%~nxi
+    if not exist "%GITDIR%" mkdir %GITDIR%
+
     curl --fail --location --silent -O --output-dir %GITDIR% %GITLINK%
-    if exist %GITDIR%%GITZIP% (
+    if "%ERRORLEVEL%" EQU 0 echo "git baixado!"
+    if exist "%GITDIR%%GITZIP%" (
         cd %GITDIR%
         tar -xf %GITZIP%
         del %GITZIP%
     )
-    REM Add git to PATH
-    setx PATH %PATH%;%GITDIR%cmd
 )
 
 set GVIMDIR=%USERPROFILE%\Documents\gvim\
-set GVIMLINK=https://github.com/vim/vim-win32-installer/releases/download/v9.2.0612/gvim_9.2.0612_x64.zip
-set GVIMZIP=gvim_9.2.0612_x64.zip
+set GVIMLINK=https://github.com/vim/vim-win32-installer/releases/download/v%GVIMVERSION%/gvim_%GVIMVERSION%_x64.zip
+for /F "delims=" %%i in ("%GVIMLINK%") do set GVIMZIP=%%~nxi
 
-if not exist %GVIMDIR% (
-    mkdir %GVIMDIR%
-)
+if not exist "%GVIMDIR%"  mkdir %GVIMDIR%
 
 curl --fail --location --silent -O --output-dir %GVIMDIR% %GVIMLINK%
+if "%ERRORLEVEL%" EQU 0 echo "gvim baixado!"
 
-if exist %GVIMDIR%%GVIMZIP% (
+if exist "%GVIMDIR%%GVIMZIP%" (
     cd %GVIMDIR%
     tar -xf %GVIMZIP%
     cd %GVIMDIR%vim
@@ -43,11 +43,12 @@ if exist %GVIMDIR%%GVIMZIP% (
     REM git remote add nvimrc https://github.com/Andrikin/nvimrc
     git remote add nvimrc git@github.com:Andrikin/nvimrc
     git pull nvimrc gvim
-    if exist %GVIMDIR%%GVIMZIP% ( del %GVIMDIR%%GVIMZIP% )
+    if exist "%GVIMDIR%%GVIMZIP%" ( del %GVIMDIR%%GVIMZIP% )
 )
 
+echo "executando gvim!"
 REM Open gvim
-%GVIMDIR%vim\vim92\gvim.exe
+cmd.exe /c start %GVIMDIR%vim\vim92\gvim.exe
 
-endlocal
+@endlocal
 
